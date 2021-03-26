6c6361b848636b187dfa4ec425b93bc1da02d1a8
Francisco Carpio <carpiofj@gmail.com>
2021-03-26 18:06:11 +0100

    update qtile config

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -4,6 +4,7 @@
 nm-applet & # systray network
 numlockx on &
 pamac-tray &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 syncthing &
 picom -b --config  $HOME/.config/qtile/picom.conf
 
