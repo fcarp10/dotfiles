19fce7f7b26dbe7d095489d2890a197f9d3d979f
Francisco Carpio <carpiofj@gmail.com>
2021-04-27 16:53:45 +0200

    update qtile autostart

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-# xrandr --output DP1-8 --primary --mode 1920x1200 --output HDMI3 --mode 1920x1200 --left-of DP1-8
-
 # cbatticon -u 5 & # systray battery icon
 nm-applet & # systray network
 numlockx on &
 pamac-tray &
+volumeicon &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 syncthing &
 picom -b --config $HOME/.config/qtile/picom.conf
